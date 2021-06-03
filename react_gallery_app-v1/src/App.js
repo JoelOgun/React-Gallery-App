5387a41a72f04b8681fd935e8423c88d442efb00
Joel Ogunremi <joelogunremi@gmail.com>
2021-06-03 19:53:56 -0400

    First commit

---
+++ react_gallery_app-v1/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./index.css";
+//import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Search App</h1>
+        <p>This is a simple search app</p>
+      </div>
+    );
+  }
+}
+
+export default App;
