c61a602eaceb9f6fa5e6610e39943df21fb6bb5b
Joel Ogunremi <joelogunremi@gmail.com>
2021-06-14 13:17:38 -0400

    Search Form display

--- react_gallery_app-v1/src/App.js
+++ react_gallery_app-v1/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
-import "./index.css";
-//import "./App.css";
+
+//import SearchForm from "./components/SearchForm";
+import "./App.css";
+import "./css/index.css";
+
+import SearchForm from "./components/SearchForm";
 
 class App extends Component {
   render() {
@@ -8,6 +12,8 @@ class App extends Component {
       <div>
         <h1>Search App</h1>
         <p>This is a simple search app</p>
+        <SearchForm />
+        {/*<SearchForm /> */}
       </div>
     );
   }
